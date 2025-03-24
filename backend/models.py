a9d0436103960e91cb9a15518c64e8a843684e44
Bekzod <powerzver98@gmail.com>
2025-03-24 14:35:44 +0500

    added models profile

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Profile(models.Model):
+    username = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=200, null=True, blank=True, default=None)
+    lastname = models.CharField(max_length=200, null=True, blank=True, default=None)
+    middle_name = models.CharField(max_length=200, null=True, blank=True, default=None)
+    date_birth = models.DateField(null=True, blank=True, default=None)
+    phone = PhoneNumberField()
+    adress = models.CharField(max_length=200,blank=True)
+    last_job=models.CharField(max_length=200,blank=True)
+    work_exp=models.CharField(max_length=200,blank=True)
+    postion=models.CharField(max_length=200)
+    photo = models.ImageField(blank=True, upload_to='pictures/')
+
+
+
