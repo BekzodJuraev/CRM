72b8722c5d5ab20e0cfc5e57245fd24973b32e9e
Nurbek Juraev <powerzver98@gmail.com>
2025-03-30 20:32:45 +0500

    added orders model

--- backend/models.py
+++ backend/models.py
@@ -4,6 +4,15 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Profile(models.Model):
+    POSITION_CHOICES = [
+        ("manager", "Менеджер"),
+        ("admin", "Администратор"),
+        ("supplier", "Снабженец"),
+        ("chief", "Главный ЦЕХа"),
+        ("installer", "Установщик"),
+        ("technologist", "Технолог"),
+        ("designer", "Дизайнер"),
+    ]
     username = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=200, null=True, blank=True, default=None)
     lastname = models.CharField(max_length=200, null=True, blank=True, default=None)
@@ -13,8 +22,41 @@ class Profile(models.Model):
     adress = models.CharField(max_length=200,blank=True)
     last_job=models.CharField(max_length=200,blank=True)
     work_exp=models.CharField(max_length=200,blank=True)
-    postion=models.CharField(max_length=200)
+    position = models.CharField(max_length=20, choices=POSITION_CHOICES)
     photo = models.ImageField(blank=True, upload_to='pictures/')
+    approve=models.BooleanField(default=False)
 
 
 
+class Stage(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+class Orders(models.Model):
+    client=models.CharField(max_length=250)
+    adress=models.CharField(max_length=250)
+    order_sum=models.IntegerField(default=0)
+    order_predoplata=models.IntegerField(default=0)
+    category=models.CharField(max_length=250)
+    description=models.TextField()
+    phone=models.CharField(max_length=50)
+    social=models.CharField(max_length=60)
+    stage=models.ForeignKey(Stage,on_delete=models.CASCADE, related_name='order')
+    add_order=models.DateField()
+    complete_order=models.DateField()
+
+
+
+    def __str__(self):
+        return self.client
+
+
+class Consumables(models.Model):
+    add=models.CharField(max_length=50)
+    price=models.IntegerField()
+    catigories=models.CharField(max_length=50)
+    order = models.ForeignKey(Orders, on_delete=models.CASCADE, related_name='consumables')
+
+    def __str__(self):
+        return self.catigories
